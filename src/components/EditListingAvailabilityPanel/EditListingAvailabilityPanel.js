877d070dc3527c87e5a4c8d2cec4041e9bf19754
Toshi <31828146+abmgmt@users.noreply.github.com>
2019-06-05 12:01:17 -0400

    Updated defaultAvailabilityPlan to /time instead of /day and added startTime and endTime strings.

--- src/components/EditListingAvailabilityPanel/EditListingAvailabilityPanel.js
+++ src/components/EditListingAvailabilityPanel/EditListingAvailabilityPanel.js
@@ -27,15 +27,16 @@ const EditListingAvailabilityPanel = props => {
   const currentListing = ensureOwnListing(listing);
   const isPublished = currentListing.id && currentListing.attributes.state !== LISTING_STATE_DRAFT;
   const defaultAvailabilityPlan = {
-    type: 'availability-plan/day',
+    type: 'availability-plan/time',
+    timezone: 'EST',
     entries: [
-      { dayOfWeek: 'mon', seats: 1 },
-      { dayOfWeek: 'tue', seats: 1 },
-      { dayOfWeek: 'wed', seats: 1 },
-      { dayOfWeek: 'thu', seats: 1 },
-      { dayOfWeek: 'fri', seats: 1 },
-      { dayOfWeek: 'sat', seats: 1 },
-      { dayOfWeek: 'sun', seats: 1 },
+      { dayOfWeek: 'mon', seats: 24, startTime: '00:00', endTime: '23:00' },
+      { dayOfWeek: 'tue', seats: 24, startTime: '00:00', endTime: '23:00' },
+      { dayOfWeek: 'wed', seats: 24, startTime: '00:00', endTime: '23:00' },
+      { dayOfWeek: 'thu', seats: 24, startTime: '00:00', endTime: '23:00' },
+      { dayOfWeek: 'fri', seats: 24, startTime: '00:00', endTime: '23:00' },
+      { dayOfWeek: 'sat', seats: 24, startTime: '00:00', endTime: '23:00' },
+      { dayOfWeek: 'sun', seats: 24, startTime: '00:00', endTime: '23:00' },
     ],
   };
   const availabilityPlan = currentListing.attributes.availabilityPlan || defaultAvailabilityPlan;
